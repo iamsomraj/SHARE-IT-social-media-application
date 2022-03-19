77c16dc393dadbcf05d706fea1d1e370597f646c
Somraj Mukherjee <iamsomraj@gmail.com>
2022-03-19 13:18:13 +0530

    📦 NEW: Post List comp added

---
+++ client/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div
+      v-for="post in posts"
+      :key="post.id"
+      class="px-6 py-3 border-t border-b border-separate"
+    >
+      <PostListItem
+        :name="name"
+        :content="post.content"
+        :numberOfLikes="post.likesOnPost.length"
+        :date="(post.updatedAt ? post.updatedAt : post.createdAt).slice(0, 10)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  props: ['posts', 'name'],
+};
+</script>
+
+<style scoped></style>
--- client/components/ProfileBody.vue
+++ client/components/ProfileBody.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="my-2">
-    <div
-      v-for="post in posts"
-      :key="post.id"
-      class="px-6 py-3 border-t border-b border-separate"
-    >
-      <PostListItem
-        :name="name"
-        :content="post.content"
-        :numberOfLikes="post.likesOnPost.length"
-        :date="(post.updatedAt ? post.updatedAt : post.createdAt).slice(0, 10)"
-      />
-    </div>
+    <PostList :posts="posts" :name="name" />
   </div>
 </template>
 

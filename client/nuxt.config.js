02376586974b47a92e608d4aa23717c72b842149
Somraj Mukherjee <iamsomraj@gmail.com>
2022-03-20 22:45:16 +0530

    📦 NEW: title for page

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'client',
+    title: 'SHARE IT - share thoughts, posts',
     htmlAttrs: {
       lang: 'en',
     },
@@ -43,4 +43,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-}
+};

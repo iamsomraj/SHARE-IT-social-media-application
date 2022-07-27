0c5564a4eb12c9eb7a22d4026411ffd1e9de0491
Somraj Mukherjee <iamsomraj@gmail.com>
2022-07-27 13:39:33 +0530

    👌 IMPROVE: added crypto config for passwords

--- server/utils/helpers/index.js
+++ server/utils/helpers/index.js
@@ -3,14 +3,19 @@ const dotenv = require("dotenv");
 const jsonwebtoken = require("jsonwebtoken");
 dotenv.config();
 
-const SALT = process.env.SALT.toString("hex");
+const BASE_RADIX_FOR_CRYPTO = "hex";
+const BASE_DIGEST_ALGORITHM_FOR_CRYPTO = "sha512";
+const BASE_ITERATIONS_FOR_CRYPTO = 1000;
+const BASE_KEY_LENGTH_FOR_CRYPTO = 64;
+
+const SALT = process.env.SALT.toString(BASE_RADIX_FOR_CRYPTO);
 
 const hash = (password) => {
-  return crypto.pbkdf2Sync(password, SALT, 1000, 64, `sha512`).toString(`hex`);
+  return crypto.pbkdf2Sync(password, SALT, BASE_ITERATIONS_FOR_CRYPTO, BASE_KEY_LENGTH_FOR_CRYPTO, BASE_DIGEST_ALGORITHM_FOR_CRYPTO).toString(BASE_RADIX_FOR_CRYPTO);
 };
 
 const validateHash = (password, already_hashed_password) => {
-  var current_hash = crypto.pbkdf2Sync(password, SALT, 1000, 64, `sha512`).toString(`hex`);
+  var current_hash = crypto.pbkdf2Sync(password, SALT, BASE_ITERATIONS_FOR_CRYPTO, BASE_KEY_LENGTH_FOR_CRYPTO, BASE_DIGEST_ALGORITHM_FOR_CRYPTO).toString(BASE_RADIX_FOR_CRYPTO);
   return current_hash === already_hashed_password;
 };
 

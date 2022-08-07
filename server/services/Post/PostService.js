f29d16bd5d2328e48cf354de7723206ebe225563
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-07 20:37:57 +0530

    👌 IMPROVE: fixed post feed for users

--- server/services/Post/PostService.js
+++ server/services/Post/PostService.js
@@ -145,7 +145,14 @@ class PostService extends RootService {
     const followingIds = followingRecords.map((record) => record.followed_id);
 
     /* BEGIN: FETCH POSTS OF FOLLOWINGS */
-    const postRecords = await PostsModel.query().withGraphFetched("[post_likes.creator(defaultSelects), post_stats, creator(defaultSelects)]").whereIn("created_by", followingIds).orderBy("created_at", "desc").orderBy("updated_at", "desc");
+    const postRecords = await PostsModel.query()
+      .withGraphFetched("[post_likes.creator(defaultSelects), post_stats, creator(defaultSelects)]")
+      .where((buider) => {
+        buider.orWhereIn("created_by", followingIds);
+        buider.orWhere("created_by", user.id);
+      })
+      .orderBy("created_at", "desc")
+      .orderBy("created_at", "desc");
     /* END: FETCH POSTS OF FOLLOWINGS */
 
     if (!postRecords) this.raiseError(HTTP_CODES.INTERNAL_SERVER_ERROR, PERSON_ERROR_MESSAGES.POST_FEED_FAILURE);

8fd54b9fee07c087226b81ffb2572c0d60939a04
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-07 16:16:38 +0530

    👌 IMPROVE: added search input and search results

--- client/components/persons/SearchPeople.vue
+++ client/components/persons/SearchPeople.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="flex w-full flex-col items-start justify-center md:w-1/2">
-    Search People
+    <!-- BEGIN: SEARCH INPUT -->
+    <div></div>
+    <!-- END: SEARCH INPUT -->
+
+    <!-- BEGIN: SEARCH RESULTS -->
+    <div></div>
+    <!-- END: SEARCH RESULTS -->
   </div>
 </template>
 

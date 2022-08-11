ced649a02b9f25630df8d49778d17bab5b4132ee
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-11 17:08:12 +0530

    👌 IMPROVE: refactored theme store with dark

--- client/store/theme.js
+++ client/store/theme.js
@@ -10,19 +10,68 @@ export const state = () => ({
 
 export const getters = {
   theme: (state) => state.theme,
+  isDarkTheme: (state) => state.theme === THEMES.DARK,
+  isLightTheme: (state) => state.theme === THEMES.LIGHT,
 };
 
 export const actions = {
   handleTheme() {
     if (
-      localStorage.getItem('color-theme') === 'dark' ||
-      (!('color-theme' in localStorage) &&
+      localStorage.getItem(LOCAL_STORAGE_THEME_KEY) === THEMES.DARK ||
+      (!(LOCAL_STORAGE_THEME_KEY in localStorage) &&
         window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
-      document.documentElement.classList.add('dark');
+      document.documentElement.classList.add(THEMES.DARK);
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove(THEMES.DARK);
     }
+  },
+  setTheme({ commit }, theme) {
+    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+    var themeToggleLightIcon = document.getElementById(
+      'theme-toggle-light-icon'
+    );
+
+    // Change the icons inside the button based on previous settings
+    if (
+      localStorage.getItem(LOCAL_STORAGE_THEME_KEY) === THEMES.DARK ||
+      (!(LOCAL_STORAGE_THEME_KEY in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      themeToggleLightIcon.classList.remove('hidden');
+    } else {
+      themeToggleDarkIcon.classList.remove('hidden');
+    }
+
+    var themeToggleBtn = document.getElementById('theme-toggle');
+
+    themeToggleBtn.addEventListener('click', function () {
+      // toggle icons inside button
+      themeToggleDarkIcon.classList.toggle('hidden');
+      themeToggleLightIcon.classList.toggle('hidden');
+
+      // if set via local storage previously
+      if (localStorage.getItem(LOCAL_STORAGE_THEME_KEY)) {
+        if (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) === 'light') {
+          document.documentElement.classList.add(THEMES.DARK);
+          localStorage.setItem(LOCAL_STORAGE_THEME_KEY, THEMES.DARK);
+        } else {
+          document.documentElement.classList.remove(THEMES.DARK);
+          localStorage.setItem(LOCAL_STORAGE_THEME_KEY, 'light');
+        }
+
+        // if NOT set via local storage previously
+      } else {
+        if (document.documentElement.classList.contains(THEMES.DARK)) {
+          document.documentElement.classList.remove(THEMES.DARK);
+          localStorage.setItem(LOCAL_STORAGE_THEME_KEY, 'light');
+        } else {
+          document.documentElement.classList.add(THEMES.DARK);
+          localStorage.setItem(LOCAL_STORAGE_THEME_KEY, THEMES.DARK);
+        }
+      }
+    });
+    commit('setTheme', theme);
   },
 };
 

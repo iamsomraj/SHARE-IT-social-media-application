2352f0324bd0670fc659805a0318c30379277ece
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-17 13:38:03 +0530

    👌 IMPROVE: icons updated for dark mode

--- client/components/user-interfaces/ThemeButton.vue
+++ client/components/user-interfaces/ThemeButton.vue
@@ -1,11 +1,11 @@
 <template>
   <div @click="toggleTheme">
     <moon-icon
-      class="h-6 w-6 cursor-pointer fill-slate-300 transition-all duration-500 hover:fill-blue-400 active:fill-blue-400"
+      class="h-6 w-6 cursor-pointer fill-slate-300 transition-all duration-300 hover:fill-blue-400 active:fill-blue-400 dark:fill-slate-600 dark:hover:fill-blue-400 dark:active:fill-blue-400"
       :class="{ hidden: !isDarkMode }"
     ></moon-icon>
     <sun-icon
-      class="h-6 w-6 cursor-pointer stroke-slate-300 transition-all duration-500 hover:stroke-blue-400 active:stroke-blue-400"
+      class="h-6 w-6 cursor-pointer stroke-slate-300 transition-all duration-300 hover:stroke-blue-400 active:stroke-blue-400 dark:hover:stroke-blue-400 dark:active:stroke-blue-400"
       :class="{ hidden: isDarkMode }"
     ></sun-icon>
   </div>

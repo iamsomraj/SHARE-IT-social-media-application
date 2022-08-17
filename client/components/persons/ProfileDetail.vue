f9bb6f1c5eccd247819fecab6c4282a168c9d6f2
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-17 14:12:57 +0530

    👌 IMPROVE: initial dark mode change in profile detail, picture and stats

--- client/components/persons/ProfileDetail.vue
+++ client/components/persons/ProfileDetail.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col items-start justify-center space-y-2">
-    <div class="text-left text-lg font-bold capitalize text-slate-600">
+    <div
+      class="text-left text-lg font-bold capitalize text-slate-600 dark:text-slate-200"
+    >
       {{ name }}
     </div>
     <div class="text-left text-xs font-light text-slate-400">
--- client/components/persons/ProfileStats.vue
+++ client/components/persons/ProfileStats.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="col-span-8 flex items-center justify-between">
-    <div class="px-1 py-2 text-center text-slate-600">
+    <div class="px-1 py-2 text-center text-slate-600 dark:text-slate-200">
       <div>{{ numberOfPosts }}</div>
       <div class="text-xs text-slate-400">Posts</div>
     </div>
-    <div class="px-1 py-2 text-center text-slate-600">
+    <div class="px-1 py-2 text-center text-slate-600 dark:text-slate-200">
       <div>{{ numberOfFollowers }}</div>
       <div class="text-xs text-slate-400">Followers</div>
     </div>
-    <div class="px-1 py-2 text-center">
+    <div class="px-1 py-2 text-center text-slate-600 dark:text-slate-200">
       <div>{{ numberOfFollowings }}</div>
       <div class="text-xs text-slate-400">Followings</div>
     </div>

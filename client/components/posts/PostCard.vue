e4e42c2fc167e3ce604123280089ad1664ed7c59
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-19 23:08:42 +0530

    👌 IMPROVE: added like text update

--- client/components/posts/PostCard.vue
+++ client/components/posts/PostCard.vue
@@ -2,13 +2,15 @@
   <div
     class="flex w-full flex-col items-start justify-center space-y-4 border-t border-b transition-all duration-300 dark:border-slate-600 md:w-1/2 md:rounded-xl md:border"
   >
-    <!-- BEGIN: POST LIST ITEM HEADER -->
-    <div class="flex items-center justify-start space-x-4 px-6 pt-2">
+    <!-- BEGIN: POST CARD HEADER -->
+    <div
+      class="flex w-full items-center justify-start space-x-4 border-b px-6 py-4 dark:border-slate-600"
+    >
       <!-- BEGIN: POST PROFILE PICTURE -->
       <profile-picture
         :uuid="ownerUUID"
         :name="ownerName"
-        class="h-8 w-8 text-5xl"
+        class="h-8 w-8 shrink-0 text-5xl"
       ></profile-picture>
       <!-- END: POST PROFILE PICTURE -->
 
@@ -17,25 +19,25 @@
         <!-- BEGIN: POST OWNER NAME -->
         <div
           @click="$router.push(`/profile/${ownerUUID}`)"
-          class="cursor-pointer text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200"
+          class="cursor-pointer break-words text-3xl text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200"
         >
           {{ ownerName }}
         </div>
         <!-- END: POST OWNER NAME -->
 
         <!-- BEGIN: POST TIME -->
-        <div class="text-xs font-light text-slate-400">{{ time }}</div>
+        <div class="text-sm font-light text-slate-400">{{ time }}</div>
         <!-- END: POST TIME -->
       </div>
       <!-- END: POST OWNER NAME AND TIME -->
     </div>
-    <!-- END: POST LIST ITEM HEADER -->
+    <!-- END: POST CARD HEADER -->
 
-    <!-- BEGIN: POST LIST ITEM CONTENT -->
+    <!-- BEGIN: POST CARD CONTENT -->
     <div class="flex flex-col items-start justify-center space-y-4 px-6">
       <!-- BEGIN: POST CONTENT -->
       <div
-        class="cursor-pointer break-words text-2xl text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200"
+        class="cursor-pointer break-words text-2xl text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200 md:text-4xl"
         @click="$router.push(`/post/${uuid}`)"
       >
         {{ content }}
@@ -44,17 +46,15 @@
 
       <!-- BEGIN: NUMBER OF LIKES -->
       <div
-        class="cursor-pointer text-sm text-slate-400 underline-offset-4 hover:underline"
+        class="cursor-pointer break-words text-base text-slate-400 underline-offset-4 line-clamp-1 hover:underline"
       >
-        {{
-          numberOfLikes > 1 ? `${numberOfLikes} Likes` : `${numberOfLikes} Like`
-        }}
+        {{ likeText }}
       </div>
       <!-- END: NUMBER OF LIKES -->
     </div>
-    <!-- END: POST LIST ITEM CONTENT -->
+    <!-- END: POST CARD CONTENT -->
 
-    <!-- BEGIN: POST LIST ITEM FOOTER -->
+    <!-- BEGIN: POST CARD FOOTER -->
     <div
       class="flex w-full items-center justify-start space-x-2 border-t px-6 py-2 text-xs dark:border-t-slate-600"
     >
@@ -81,7 +81,7 @@
       </div>
       <!-- END: LIKE ICON -->
     </div>
-    <!-- END: POST LIST ITEM FOOTER -->
+    <!-- END: POST CARD FOOTER -->
   </div>
 </template>
 
@@ -102,6 +102,19 @@ export default {
     'postLikes',
   ],
   computed: {
+    likeText() {
+      const names = this.postLikes
+        .slice(0, 2)
+        .map((post) => post.creator.name)
+        .join(', ');
+      if (this.postLikes.length > 2) {
+        return `${names} and ${this.postLikes.length - 2} ${
+          this.postLikes.length - 2 > 1 ? 'others' : 'other'
+        } liked this`;
+      } else {
+        return `${names} liked this`;
+      }
+    },
     loggedInUserUUID() {
       return this.$store.getters['auth/uuid'];
     },

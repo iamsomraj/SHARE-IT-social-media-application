3baa84fbab66a966ab334f6c050cbc6504db4ac6
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-19 22:22:37 +0530

    👌 IMPROVE: added post card component

---
+++ client/components/posts/PostCard.vue
@@ -0,0 +1,126 @@
+<template>
+  <div
+    class="flex w-full flex-col items-start justify-center space-y-4 border-t border-b transition-all duration-300 dark:border-slate-600 md:w-1/2 md:rounded-xl md:border"
+  >
+    <!-- BEGIN: POST LIST ITEM HEADER -->
+    <div class="flex items-center justify-start space-x-4 px-6 pt-2">
+      <!-- BEGIN: POST PROFILE PICTURE -->
+      <profile-picture
+        :uuid="ownerUUID"
+        :name="ownerName"
+        class="h-8 w-8 text-5xl"
+      ></profile-picture>
+      <!-- END: POST PROFILE PICTURE -->
+
+      <!-- BEGIN: POST OWNER NAME AND TIME -->
+      <div class="flex flex-col items-start justify-center font-bold">
+        <!-- BEGIN: POST OWNER NAME -->
+        <div
+          @click="$router.push(`/profile/${ownerUUID}`)"
+          class="cursor-pointer text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200"
+        >
+          {{ ownerName }}
+        </div>
+        <!-- END: POST OWNER NAME -->
+
+        <!-- BEGIN: POST TIME -->
+        <div class="text-xs font-light text-slate-400">{{ time }}</div>
+        <!-- END: POST TIME -->
+      </div>
+      <!-- END: POST OWNER NAME AND TIME -->
+    </div>
+    <!-- END: POST LIST ITEM HEADER -->
+
+    <!-- BEGIN: POST LIST ITEM CONTENT -->
+    <div class="flex flex-col items-start justify-center space-y-4 px-6">
+      <!-- BEGIN: POST CONTENT -->
+      <div
+        class="cursor-pointer break-words text-2xl text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200"
+        @click="$router.push(`/post/${uuid}`)"
+      >
+        {{ content }}
+      </div>
+      <!-- END: POST CONTENT -->
+
+      <!-- BEGIN: NUMBER OF LIKES -->
+      <div
+        class="cursor-pointer text-sm text-slate-400 underline-offset-4 hover:underline"
+      >
+        {{
+          numberOfLikes > 1 ? `${numberOfLikes} Likes` : `${numberOfLikes} Like`
+        }}
+      </div>
+      <!-- END: NUMBER OF LIKES -->
+    </div>
+    <!-- END: POST LIST ITEM CONTENT -->
+
+    <!-- BEGIN: POST LIST ITEM FOOTER -->
+    <div
+      class="flex w-full items-center justify-start space-x-2 border-t px-6 py-2 text-xs dark:border-t-slate-600"
+    >
+      <!-- BEGIN: LIKE ICON -->
+      <div
+        v-if="!isLiked"
+        class="flex cursor-pointer items-center justify-center space-x-4"
+        @click="onPostLike(uuid)"
+      >
+        <heart-icon
+          class="fill-transparent stroke-slate-400 hover:fill-slate-400 active:fill-slate-400"
+        ></heart-icon>
+      </div>
+      <!-- END: LIKE ICON -->
+      <!-- BEGIN: LIKE ICON -->
+      <div
+        v-else
+        class="flex cursor-pointer items-center justify-center space-x-4"
+        @click="onPostUnlike(uuid)"
+      >
+        <heart-icon
+          class="fill-red-400 stroke-red-400 hover:fill-red-200 hover:stroke-red-200 active:stroke-red-200"
+        ></heart-icon>
+      </div>
+      <!-- END: LIKE ICON -->
+    </div>
+    <!-- END: POST LIST ITEM FOOTER -->
+  </div>
+</template>
+
+<script>
+import ProfilePicture from '../persons/ProfilePicture.vue';
+import HeartIcon from './../assets/HeartIcon.vue';
+export default {
+  name: 'PostCard',
+  props: [
+    'uuid',
+    'id',
+    'ownerName',
+    'ownerId',
+    'ownerUUID',
+    'content',
+    'numberOfLikes',
+    'time',
+    'postLikes',
+  ],
+  computed: {
+    loggedInUserUUID() {
+      return this.$store.getters['auth/uuid'];
+    },
+    isLiked() {
+      return this.postLikes?.some(
+        (likeRecord) => likeRecord?.creator?.uuid === this.loggedInUserUUID
+      );
+    },
+  },
+  methods: {
+    onPostLike(uuid) {
+      this.$emit('onPostLike', uuid);
+    },
+    onPostUnlike(uuid) {
+      this.$emit('onPostUnlike', uuid);
+    },
+  },
+  components: { ProfilePicture, HeartIcon },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- client/pages/post/_uuid.vue
+++ client/pages/post/_uuid.vue
@@ -1,18 +1,41 @@
 <template>
   <div
-    class="min-h-screen w-full pt-4 pb-16 text-slate-600 dark:bg-slate-800 dark:text-slate-200 md:py-8"
+    class="flex min-h-screen w-full items-start justify-center pt-4 pb-16 text-slate-600 dark:bg-slate-800 dark:text-slate-200 md:py-8"
   >
-    Post
+    <post-card
+      v-if="post"
+      :key="post.uuid"
+      :id="post.uuid"
+      :uuid="post.uuid"
+      :ownerName="post.creator.name"
+      :ownerId="post.creator.id"
+      :ownerUUID="post.creator.uuid"
+      :postLikes="post.post_likes"
+      :content="post.content"
+      :numberOfLikes="post.post_stats.like_count"
+      :time="time(post.updated_at, post.created_at)"
+      @onPostLike="onPostLike"
+      @onPostUnlike="onPostUnlike"
+    ></post-card>
   </div>
 </template>
 
 <script>
+import PostCard from '../../components/posts/PostCard.vue';
+import { getTime } from '../../util/helpers';
 export default {
   name: 'PostPage',
   middleware: 'authenticated',
+  data() {
+    return {
+      loading: false,
+    };
+  },
   async fetch() {
+    this.loading = true;
     const payload = { uuid: this.uuid, token: this.token };
     await this.$store.dispatch('post/fetchPost', payload);
+    this.loading = false;
   },
   computed: {
     token() {
@@ -25,5 +48,17 @@ export default {
       return this.$store.getters['post/post'];
     },
   },
+  methods: {
+    onPostLike(uuid) {
+      this.$emit('onPostLike', uuid);
+    },
+    onPostUnlike(uuid) {
+      this.$emit('onPostUnlike', uuid);
+    },
+    time(updated_at, created_at) {
+      return getTime(updated_at ? updated_at : created_at);
+    },
+  },
+  components: { PostCard },
 };
 </script>

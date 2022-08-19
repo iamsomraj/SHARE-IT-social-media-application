bfa1210a2e84061b2f370965a65b0595f8eac034
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-19 19:09:23 +0530

    📦 NEW: post page added

---
+++ client/pages/post/_uuid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="min-h-screen w-full pt-4 pb-16 text-slate-600 dark:bg-slate-800 dark:text-slate-200 md:py-8"
+  >
+    Post Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostPage',
+  middleware: 'authenticated',
+};
+</script>

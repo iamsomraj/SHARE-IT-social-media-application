f03af390eb6732886f7c9f6c1ea69a90200f7d20
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-22 20:20:12 +0530

    👌 IMPROVE: added fav icon to post card header

--- client/components/posts/post-card/PostCardHeader.vue
+++ client/components/posts/post-card/PostCardHeader.vue
@@ -1,38 +1,50 @@
 <template>
   <!-- BEGIN: POST CARD HEADER -->
   <div
-    class="flex w-full items-center justify-start space-x-4 border-b px-6 py-4 dark:border-slate-600"
+    class="flex w-full items-center justify-between space-x-4 border-b px-6 py-4 dark:border-slate-600"
   >
-    <!-- BEGIN: POST PROFILE PICTURE -->
-    <profile-picture
-      :uuid="ownerUUID"
-      :name="ownerName"
-      class="h-8 w-8 shrink-0 text-5xl"
-    ></profile-picture>
-    <!-- END: POST PROFILE PICTURE -->
+    <div class="flex items-center justify-center space-x-4">
+      <!-- BEGIN: POST PROFILE PICTURE -->
+      <profile-picture
+        :uuid="ownerUUID"
+        :name="ownerName"
+        class="h-8 w-8 shrink-0 text-5xl"
+      ></profile-picture>
+      <!-- END: POST PROFILE PICTURE -->
 
-    <!-- BEGIN: POST OWNER NAME AND TIME -->
-    <div class="flex flex-col items-start justify-center font-bold">
-      <!-- BEGIN: POST OWNER NAME -->
-      <div
-        @click="$router.push(`/profile/${ownerUUID}`)"
-        class="cursor-pointer break-words text-3xl text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200"
-      >
-        {{ ownerName }}
+      <!-- BEGIN: POST OWNER NAME AND TIME -->
+      <div class="flex flex-col items-start justify-center font-bold">
+        <!-- BEGIN: POST OWNER NAME -->
+        <div
+          @click="$router.push(`/profile/${ownerUUID}`)"
+          class="cursor-pointer break-words text-3xl text-slate-600 transition-all duration-300 hover:underline dark:text-slate-200"
+        >
+          {{ ownerName }}
+        </div>
+        <!-- END: POST OWNER NAME -->
+
+        <!-- BEGIN: POST TIME -->
+        <div class="text-sm font-light text-slate-400">{{ time }}</div>
+        <!-- END: POST TIME -->
       </div>
-      <!-- END: POST OWNER NAME -->
-
-      <!-- BEGIN: POST TIME -->
-      <div class="text-sm font-light text-slate-400">{{ time }}</div>
-      <!-- END: POST TIME -->
+      <!-- END: POST OWNER NAME AND TIME -->
     </div>
-    <!-- END: POST OWNER NAME AND TIME -->
+
+    <favourite-icon
+      class="h-6 w-6 stroke-slate-600 dark:stroke-slate-200"
+    ></favourite-icon>
+    <!-- 
+    <favourite-icon
+      class="h-6 w-6 fill-yellow-200 stroke-yellow-200"
+    ></favourite-icon> -->
   </div>
   <!-- END: POST CARD HEADER -->
 </template>
 
 <script>
 import ProfilePicture from '../../persons/ProfilePicture.vue';
+import FavouriteIcon from '../../assets/FavouriteIcon.vue';
+
 export default {
   name: 'PostCardHeader',
   props: {
@@ -51,6 +63,7 @@ export default {
   },
   components: {
     ProfilePicture,
+    FavouriteIcon,
   },
 };
 </script>

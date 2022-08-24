da11b18600db5b7eb637024e9b0ab9f8ea2d69f3
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-24 22:49:05 +0530

    👌 IMPROVE: added life cycle handling

--- client/components/posts/favourites/FavouriteCard.vue
+++ client/components/posts/favourites/FavouriteCard.vue
@@ -1,17 +1,23 @@
 <template>
-  <teleport to="body">
-    <div
-      class="fixed inset-0 z-20 flex max-h-screen w-screen items-center justify-center bg-white transition-all duration-300 dark:bg-slate-800 dark:text-slate-200"
-      @click="$emit('dismiss')"
-    >
-      Hello World
-    </div>
-  </teleport>
+  <div
+    class="fixed inset-0 z-20 flex max-h-screen w-screen items-center justify-center bg-white transition-all duration-300 dark:bg-slate-800 dark:text-slate-200"
+    @click="$emit('dismiss')"
+  >
+    Hello World
+  </div>
 </template>
 
 <script>
 export default {
   name: 'FavouriteCard',
   emits: ['dismiss'],
+  mounted() {
+    const body = document.querySelector('body');
+    body.style.overflow = 'hidden';
+  },
+  beforeDestroy() {
+    const body = document.querySelector('body');
+    body.style.overflow = 'auto';
+  },
 };
 </script>

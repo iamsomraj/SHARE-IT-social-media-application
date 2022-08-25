565002f538ba22c77dfbaf31c56c6c3a1b260f78
Somraj Mukherjee <iamsomraj@gmail.com>
2022-08-25 20:31:32 +0530

    👌 IMPROVE: added post content to show

--- client/components/posts/favourites/FavouriteCard.vue
+++ client/components/posts/favourites/FavouriteCard.vue
@@ -2,14 +2,26 @@
   <div
     class="fixed inset-0 z-20 flex max-h-screen w-screen items-center justify-center bg-white transition-all duration-300 dark:bg-slate-800 dark:text-slate-200"
   >
-    <div
-      class="absolute top-8 right-8 cursor-pointer rounded-xl border-2 border-transparent bg-inherit hover:border-2 hover:border-slate-200 hover:shadow-md dark:hover:border-slate-600"
-      @click="$emit('dismiss')"
-    >
-      <CrossIcon class="m-2 h-6 w-6 stroke-2" />
-    </div>
-    <div class="mt-10">
-      {{ posts.find((post) => post.id === selectedId) }}
+    <div class="flex h-full w-full flex-col items-center justify-start">
+      <div class="flex w-full items-center justify-between px-6 py-6 md:px-12">
+        <div class="cursor-pointer" @click="$emit('dismiss')">
+          <div
+            class="flex items-center justify-center space-x-2 rounded text-2xl font-extrabold tracking-tighter"
+          >
+            <span class="text-blue-400">SHARE</span>
+            <span class="text-yellow-400">IT</span>
+          </div>
+        </div>
+        <div
+          class="cursor-pointer rounded-xl border-2 border-transparent bg-inherit hover:border-2 hover:border-slate-200 hover:shadow-md dark:hover:border-slate-600"
+          @click="$emit('dismiss')"
+        >
+          <CrossIcon class="m-2 h-6 w-6 stroke-2" />
+        </div>
+      </div>
+      <div class="w-full break-words px-6 md:w-1/4">
+        {{ postContent }}
+      </div>
     </div>
   </div>
 </template>
@@ -35,6 +47,14 @@ export default {
   beforeDestroy() {
     const body = document.querySelector('body');
     body.style.overflow = 'auto';
+  },
+  computed: {
+    selectedPost() {
+      return this.posts.find((post) => post.id === this.selectedId);
+    },
+    postContent() {
+      return this.selectedPost.content;
+    },
   },
   emits: ['dismiss'],
   components: { CrossIcon },

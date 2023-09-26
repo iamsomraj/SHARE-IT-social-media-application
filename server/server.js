f39e7b51a530c2f3f4398dbae9ec06f71d8a2a54
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-26 22:07:52 +0530

    👌 IMPROVE: added cors env check

--- server/server.js
+++ server/server.js
@@ -1,22 +1,27 @@
-const knex = require("./config/db-config.js");
-const express = require("express");
-const cors = require("cors");
-const _ = require("colors");
+const knex = require('./config/db-config.js');
+const express = require('express');
+const cors = require('cors');
+const _ = require('colors');
 
-const personRoutes = require("./routes/personRoutes.js");
-const postRoutes = require("./routes/postRoutes.js");
-const authRoutes = require("./routes/authRoutes.js");
-const { pageNotFound, errorHandler } = require("./middlewares/error.js");
-const ENVIRONMENT = require("./utils/constants/environments.js");
+const personRoutes = require('./routes/personRoutes.js');
+const postRoutes = require('./routes/postRoutes.js');
+const authRoutes = require('./routes/authRoutes.js');
+const { pageNotFound, errorHandler } = require('./middlewares/error.js');
+const ENVIRONMENT = require('./utils/constants/environments.js');
 
 const app = express();
 
-if (!ENVIRONMENT.IS_PRODUCTION) app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_CLIENT_ORIGIN : process.env.DEVELOPMENT_CLIENT_ORIGIN],
+    methods: ['POST', 'GET', 'PUT', 'DELETE'],
+  })
+);
 
 app.use(express.json());
-app.use("/api/v1/persons", personRoutes);
-app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/auth", authRoutes);
+app.use('/api/v1/persons', personRoutes);
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/auth', authRoutes);
 
 app.use(pageNotFound);
 app.use(errorHandler);

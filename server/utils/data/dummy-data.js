d82dcc3026f6d809360577e574cee0c788357859
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-26 22:52:09 +0530

    👌 IMPROVE: changed passwords

--- server/utils/data/dummy-data.js
+++ server/utils/data/dummy-data.js
@@ -1,9 +1,9 @@
 const { hash } = require("../helpers/index.js");
 
 const person_data = [
-  { name: "Sheldon Cooper", email: "sheldon@example.com", password: hash("1234"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
-  { name: "Leonard Hofstadter", email: "leonard@example.com", password: hash("1234"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
-  { name: "Penny", email: "penny@example.com", password: hash("1234"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: "Sheldon Cooper", email: "sheldon@example.com", password: hash("123456"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: "Leonard Hofstadter", email: "leonard@example.com", password: hash("123456"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: "Penny", email: "penny@example.com", password: hash("123456"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
   { name: "Somraj Mukherjee", email: "iamsomraj@example.com", password: hash("123456"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
 ];
 

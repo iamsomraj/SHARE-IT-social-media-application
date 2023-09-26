6d899af85bb51fa31f3c853fcbd49a6432581940
Somraj Mukherjee <iamsomraj@gmail.com>
2023-09-26 21:19:49 +0530

    👌 IMPROVE: added dummy data

--- server/utils/data/dummy-data.js
+++ server/utils/data/dummy-data.js
@@ -4,6 +4,7 @@ const person_data = [
   { name: "Sheldon Cooper", email: "sheldon@example.com", password: hash("1234"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
   { name: "Leonard Hofstadter", email: "leonard@example.com", password: hash("1234"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
   { name: "Penny", email: "penny@example.com", password: hash("1234"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: "Somraj Mukherjee", email: "iamsomraj@example.com", password: hash("123456"), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
 ];
 
 const data = { person_data };

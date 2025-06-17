05eb7500bf595648fd5441adc845e8f546e0b695
Somraj Mukherjee <iamsomraj@gmail.com>
2025-06-17 11:23:00 +0530

    👌 IMPROVE: enhance seeder functionality by creating a more complex follower network, generating random posts, and updating person stats with accurate post counts; add new dummy person data for better testing

--- server/utils/data/dummy-data.js
+++ server/utils/data/dummy-data.js
@@ -4,6 +4,10 @@ const person_data = [
   { name: 'Sheldon Cooper', email: 'sheldon@example.com', password: hash('123456'), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
   { name: 'Leonard Hofstadter', email: 'leonard@example.com', password: hash('123456'), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
   { name: 'Penny', email: 'penny@example.com', password: hash('123456'), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: 'Howard Wolowitz', email: 'howard@example.com', password: hash('123456'), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: 'Rajesh Koothrappali', email: 'raj@example.com', password: hash('123456'), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: 'Bernadette Rostenkowski', email: 'bernadette@example.com', password: hash('123456'), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
+  { name: 'Amy Farrah Fowler', email: 'amy@example.com', password: hash('123456'), created_at: new Date().toISOString(), updated_at: new Date().toISOString(), is_deleted: false },
 ];
 
 const data = { person_data };
